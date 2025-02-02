31bf80913cc84791c0c229a384313f6c651c6b72
Ruban <65346010+sk-ruban@users.noreply.github.com>
2025-02-02 15:08:33 +0800

    Add astro islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astro-bkl.pages.dev"
+  site: "https://astro-bkl.pages.dev",
+  integrations: [preact()]
 });
